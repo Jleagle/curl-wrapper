1302bb237624aba1dcbb8695c61dfb0e0a6bacc4
Jleagle <jimeagle@gmail.com>
2015-11-27 01:01:35 +0000

    Add docblock

--- src/Exceptions/CurlException.php
+++ src/Exceptions/CurlException.php
@@ -5,13 +5,22 @@ use Jleagle\CurlWrapper\Response;
 
 class CurlException extends \Exception
 {
+  /**
+   * @var Response|null
+   */
   protected $_response;
 
+  /**
+   * @param Response $response
+   */
   public function setResponse(Response $response)
   {
     $this->_response = $response;
   }
 
+  /**
+   * @return Response|null
+   */
   public function getResponse()
   {
     return $this->_response;
